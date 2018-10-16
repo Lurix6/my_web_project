622c86ff10c038888ddba6d31362100abdf3b033
Lurix6 <vasyapopovich23@gmail.com>
2018-10-16 09:04:02 +0300

    Add music.html and DB.js

---
+++ assets/DB/DB.js
@@ -0,0 +1,46 @@
+export let Account = [
+{   id:'0000',
+	name:'Vasya Popovich',
+	img:'../img/account/0000/img',
+	rating:'10',
+	background:'../img/account/0000/img',
+	personalDate : [{
+		country:'Україна',
+		city:'Ужгород',
+		language:'Українська',
+		birthday:'19 жовтня 1997'	
+	}],
+	music:[{
+		executor:'Alan Walker',
+		title:'Darkside (feat. Au/Ra and Tomine Harket)',
+		url:'http://taimp3nhanh.com/stream/stream.php?id=480275022'
+	},{
+		executor:'Alan Walker',
+		title:'New Day (Official Video) [New Song 2018]',
+		url:'http://taimp3nhanh.com/stream/stream.php?id=473944656'
+	},{
+		executor:'Alan Walker',
+		title:'The Spectre',
+		url:'http://taimp3nhanh.com/stream/stream.php?id=309831866'
+	},{
+		executor:'Alan Walker',
+		title:'Fade [NCS Release]',
+		url:'http://taimp3nhanh.com/stream/stream.php?id=177671751'
+	},{
+		executor:'Alan Walker',
+		title:'Spectre [NCS Release]',
+		url:'http://taimp3nhanh.com/stream/stream.php?id=184757886'
+	},{
+		executor:'Alan Walker',
+		title:'ing Me To Sleep (Marshmello Remix)',
+		url:'http://taimp3nhanh.com/stream/stream.php?id=276826702'
+	},{
+		executor:'Alan Walker',
+		title:'Alone (Feel The Beat Edit)',
+		url:'http://taimp3nhanh.com/stream/stream.php?id=295928473'
+	}
+
+
+}
+]
+
